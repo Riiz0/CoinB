fb5f52765398cef4812f3b0aebaf12298ad6ae9b
Riiz0 <shawnrizo@hotmail.com>
2023-07-11 19:43:38 -0400

    10. (Chore) Setting Transcation Fee in Token Contract

--- contracts/BUNIMEToken.sol
+++ contracts/BUNIMEToken.sol
@@ -12,12 +12,15 @@ import './BUNINFTContract.sol';
 
 contract BUNIMEToken is ERC20, Ownable{
     BUNINFTContract public nftContract;
+    uint256 public transactionFee;
 
     constructor() ERC20("BUNIME", "BUNI") {
         _mint(msg.sender, 69420000000000 * 10 ** decimals());
+        transactionFee = 6942; //Set the transaction fee to 0.732% (732 / 1,000,000)
     }
 
-    function burn(uint256 value) external {
+    function burn(uint256 value) 
+    external {
         _burn(msg.sender, value);
     }
 
@@ -25,7 +28,25 @@ contract BUNIMEToken is ERC20, Ownable{
         nftContract.mintNFT;
     }
 
-    function transferNFT(address recipient, uint256 tokenId) external {
+    function transferNFT(address recipient, uint256 tokenId) 
+    external {
         nftContract.transferNFT(recipient, tokenId);
+    }
+
+    function transfer(adress recipient, uint256 amount) 
+    public override 
+    returns (bool) {
+        uint256 feeAmount = (amount * transactionFee) / 1000000; //Calculate the transaction fee amount (dividing by 1,000,000 for percentage calculation)
+        uint256 transferAmount = amount - feeAmount; //Calculate the actual transfer amount after deducting the fee
+    
+        _transfer(_msgSender(), recipient, transferAmount); // Transfer the actual amount
+        _transfer(_msgSender(), owner(), feeAmount); // Transfer the fee amount to the contract owner
+    
+        return true;
+    }
+
+    function setTransactionFee(uint256 fee) external onlyOwner{
+        require(fee <= 1000000, "Fee must be between 0 and 1000000"); // Fee must be between 0 and 1000000 (100%)
+        transactionFee = fee;
     }
 }
