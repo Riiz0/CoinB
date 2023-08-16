908c759d0955bd0d9f9bdad5d6e44dfb4fe4f69c
Riiz0 <shawnrizo@hotmail.com>
2023-08-16 17:24:39 -0400

    14.Continuation On HomeScreen & Navbar UI

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 function HomeScreen() {
   return (
-    <div>
-      {/* Your home screen content */}
-      <h1>Welcome to My Website</h1>
-      {/* Add more content as needed */}
+    <div className="body-top-section">
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Helmet> 
+
+      {/* Left Sidebar */}
+      <div className="left-sidebar"></div>
+      
+      {/* Main Content */}
+      <div className="main-content">
+        {/* Background image will be applied via CSS */}
+        <div className="background-container">
+          {/* Background image will be applied via CSS */}
+          <div className="section-content">
+            <h1 className="section-title">JOIN THE BUNIME REVOLUTION</h1>
+            <p className="section-subtitle">YOUR ANIME COMMUNITY CRYPTOCURRENCY</p>
+            <div className="section-buttons">
+              <button className="section-button left-button">Buy BUNI</button>
+              <button className="section-button right-button">Explore</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      {/* Right Sidebar */}
+      <div className="right-sidebar"></div>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,22 +3,33 @@ import { Link } from 'react-router-dom';
 import { Icon } from 'react-icons-kit';
 import { ic_menu } from 'react-icons-kit/md/ic_menu';
 
+// Import your logo
+import logo from '../assets/logo.png'
+
 function Navbar({ handleMenuToggle }) {
   return (
-    <nav className="navbar">
-      <Icon icon={ic_menu} size={24} onClick={handleMenuToggle} />
-      <ul className="nav-links">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/tab1">Tab 1</Link>
-        </li>
-        <li>
-          <Link to="/tab2">Tab 2</Link>
-        </li>
-        {/* Add more navigation links as needed */}
-      </ul>
+    <nav className="navbar-container">
+      <div className="navbar">
+        <div className="logo-title">
+          <img src={logo} alt="Logo" className="logo" />
+          <h1 className="title">BUNIME</h1>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/About">About</Link>
+          </li>
+          <li>
+            <Link to="/Ecosystem">Ecosystem</Link>
+          </li>
+          <li>
+          <Link to="/Buy">Buy</Link>
+          </li>
+        </ul>
+        <Icon icon={ic_menu} size={24} onClick={handleMenuToggle} className="hamburger" />
+      </div>
     </nav>
   );
 }
