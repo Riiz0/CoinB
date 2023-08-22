5a60e8f30e52cf2ddbb037776d711a6d901a1d7d
Riiz0 <shawnrizo@hotmail.com>
2023-08-22 19:02:00 -0400

    22.(Chore) Editted Ecosystem Image Animation(s)

--- src/components/HomeScreen Sections/Ecosystem.js
+++ src/components/HomeScreen Sections/Ecosystem.js
@@ -2,44 +2,44 @@ import React from "react";
 import { Link } from 'react-router-dom';
 
 function Ecosystem () {
-    return(
-        <div id="ecosystem">
-            <div className="ecosystem-content">
-                   <p className="ecosystem-subtitle">ECOSYSTEM</p>
-                   <h1 className="ecosystem-title">THE BUNIME ECOSYSTEM</h1>             
-                </div>
-                <div className="ecosystem-boxes-container">
-                    <div className="ecosystem-boxes-row">
-                      <div className="ecosystem-boxes-box">
-                        <Link to="/tokenomics" className="ecosystem-link">
-                      <div className="ecosystem-boxes-box-text">$BUNI</div>
-                      <div className="ecosystem-boxes-box-subtext">TOKENOMICS</div>                
-                        </Link>
-                    </div>
-                      <div className="ecosystem-boxes-box">
-                      <Link to="/nftcollections" className="ecosystem-link">
-                      <div className="ecosystem-boxes-box-text">NFT</div>
-                      <div className="ecosystem-boxes-box-subtext">COLLECTIONS</div>                
-                        </Link>
+  return(
+    <div id="ecosystem">
+        <div className="ecosystem-content">
+            <p className="ecosystem-subtitle">ECOSYSTEM</p>
+            <h1 className="ecosystem-title">THE BUNIME ECOSYSTEM</h1>             
+          </div>
+              <div className="ecosystem-boxes-container">
+                  <div className="ecosystem-boxes-row">
+                    <Link to="/tokenomics" className="ecosystem-link">
+                      <div className="ecosystem-boxes-box tokenomics-box">
+                        <div className="ecosystem-boxes-box-text">$BUNI</div>
+                        <div className="ecosystem-boxes-box-subtext">TOKENOMICS</div>                
                       </div>
+                    </Link>
+                    <Link to="/nftcollections" className="ecosystem-link">
+                      <div className="ecosystem-boxes-box nftcollections-box">
+                        <div className="ecosystem-boxes-box-text">NFT</div>
+                        <div className="ecosystem-boxes-box-subtext">COLLECTIONS</div>                
                     </div>
+                    </Link>
+                  </div>
                 <div className="ecosystem-boxes-row">
-                    <div className="ecosystem-boxes-box">
-                    <Link to="/whitepaper" className="ecosystem-link">
-                        <div className="ecosystem-boxes-box-text">UTILITY</div>
-                        <div className="ecosystem-boxes-box-subtext">WHITEPAPER</div>                
-                        </Link>
-                    </div>
-                        <div className="ecosystem-boxes-box">
-                        <Link to="/community" className="ecosystem-link">
-                        <div className="ecosystem-boxes-box-text">COMMUNITY</div>
-                        <div className="ecosystem-boxes-box-subtext"></div>                
-                        </Link>
-                    </div>
-                </div>
-            </div>               
-        </div>
-    );
+                  <Link to="/whitepaper" className="ecosystem-link">
+                      <div className="ecosystem-boxes-box whitepaper-box">
+                          <div className="ecosystem-boxes-box-text">UTILITY</div>
+                          <div className="ecosystem-boxes-box-subtext">WHITEPAPER</div>                
+                      </div>
+                    </Link>
+                    <Link to="/community" className="ecosystem-link">
+                        <div className="ecosystem-boxes-box community-box">
+                          <div className="ecosystem-boxes-box-text">COMMUNITY</div>
+                          <div className="ecosystem-boxes-box-subtext"></div>
+                      </div>
+                    </Link>
+            </div>
+        </div>               
+    </div>
+  );
 }
 
 export default Ecosystem;
